f09c483804b68ecb8efc4c2d0acad553a9d1ddbe
Hamideh Saadatmanesh <hamidehsaadatmanesh@gmail.com>
2020-08-21 00:14:20 -0700

    add files

--- rfc.py
+++ rfc.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
@@ -18,7 +18,7 @@ sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
-classifier = GaussianNB()
+classifier = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=0)
 classifier.fit(x_train, y_train)
 
 y_pred = classifier.predict(x_test)
